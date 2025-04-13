9ae92b37802dfb475a96e6bbd0c7f02ccbe7d2a5
sahilbelurkar810 <sahilbelurkar28@gmail.com>
2025-04-13 13:03:25 +0530

    updated readme.md

--- rural_sangam/server/config/db.js
+++ rural_sangam/server/config/db.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log(process.env.MONGO_URI);
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
 };
 
